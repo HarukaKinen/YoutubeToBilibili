325d7e833f51d701b9a3d1de559517edb25c4736
deadw1nter <65445419+deadw1nter@users.noreply.github.com>
2021-03-21 10:40:24 +0800

    Update manually_download.py

--- manually_download.py
+++ manually_download.py
@@ -3,6 +3,10 @@ from database import se, task
 
 print("url: ")
 url = input()
+if url.find("youtube.com") == -1:
+    print("not a youtube video link, stop running")
+    exit()
+
 id = url.replace("https://www.youtube.com/watch?v=", "")
 if se.query(task).filter(task.id==id).first() is None:
     print("None")
